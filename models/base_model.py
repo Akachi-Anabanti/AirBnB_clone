ef70b86c2da5b6b64c57648481adce61f9be7855
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-13 19:32:38 +0000

    base model basics

---
+++ models/base_model.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""This is the base model
+defines the base attr of the model
+and it behaviour
+"""
+import uuid
+import datetime
+from datetime import datetime, timezone
+
+
+class BaseModel:
+    """Defines the base class"""
+    def __init__(self):
+        """Initilizes the class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now(timezone.utc)
+        self.updated_at = datetime.now(timezone.utc)
+
+
+    def __str__(self):
+        """The string method"""
+        return "[{}] ".format(type(self).__name__) +\
+                "({}) ".format(self.id) +\
+                "{}".format(self.__dict__)
+
+    def save(self):
+        """updates the public method 'updated_at'
+        with the current datetime"""
+
+        self.updated_at = datetime.now(timezone.utc)
+
+    def to_dict(self):
+        """Returns a dictionary representation containing
+        all keys/values of __dict__ of the instance"""
+        dict_vals = (self.__dict__).copy()
+        dict_vals["created_at"] =\
+                dict_vals["created_at"].isoformat()
+        dict_vals["updated_at"] =\
+                dict_vals["updated_at"].isoformat()
+        dict_vals['__class__'] = type(self).__name__
+        return dict_vals
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This is a test file for the base model"""
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+class BaseModelTest(unittest.TestCase):
+
+    def setUp(self):
+        self.mod1 = BaseModel()
+        self.mod1.my_number = 10
+        self.mod1.my_name = "test base model"
+        self.mod1_json = self.mod1.to_dict()
+
+    def test_base_model_has_string_id(self):
+        self.assertIsInstance(self.mod1.id, str)
+
+    def test_base_model_has_date(self):
+        self.assertTrue(type(self.mod1.created_at) == datetime.datetime)
+        self.assertTrue(type(self.mod1.updated_at) == datetime.datetime)
+
+    def test_base_model_updates_time(self):
+        prev_update_at = self.mod1.updated_at
+        self.mod1.save()
+        new_update_at = self.mod1.updated_at
+        self.assertTrue(new_update_at > prev_update_at)
+
+    def test_base_model_converts_to_dict(self):
+        self.assertIsInstance(self.mod1_json, dict)
+
+    def test_base_model_dict_has_class_key(self):
+        self.assertIn("__class__", self.mod1_json.keys())
+
+    def test_base_model_has_instance_attribute_name_number(self):
+        self.assertIn("my_number", self.mod1_json.keys())
+        self.assertIn("my_name", self.mod1_json.keys())
