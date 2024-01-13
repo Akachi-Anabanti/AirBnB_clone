3adfc6cd218db2c65023263d84765b84658a0d7a
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-13 19:49:05 +0000

    doc to test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,30 +8,41 @@ import datetime
 class BaseModelTest(unittest.TestCase):
 
     def setUp(self):
+        """setup function"""
         self.mod1 = BaseModel()
         self.mod1.my_number = 10
         self.mod1.my_name = "test base model"
         self.mod1_json = self.mod1.to_dict()
 
     def test_base_model_has_string_id(self):
+        """tests if the id is a string"""
         self.assertIsInstance(self.mod1.id, str)
 
     def test_base_model_has_date(self):
+        """tests if the created_at and updated_at are
+        datetime objects"""
         self.assertTrue(type(self.mod1.created_at) == datetime.datetime)
         self.assertTrue(type(self.mod1.updated_at) == datetime.datetime)
 
     def test_base_model_updates_time(self):
+        """tests if the time updates when
+        save method is called"""
         prev_update_at = self.mod1.updated_at
         self.mod1.save()
         new_update_at = self.mod1.updated_at
         self.assertTrue(new_update_at > prev_update_at)
 
     def test_base_model_converts_to_dict(self):
+        """tests if the base_model succesful creates
+        a dictionary object"""
         self.assertIsInstance(self.mod1_json, dict)
 
     def test_base_model_dict_has_class_key(self):
+        """tests if the dictionary representation of the base
+        model has the '__class__' in it"""
         self.assertIn("__class__", self.mod1_json.keys())
 
     def test_base_model_has_instance_attribute_name_number(self):
+        """tests if the dictionary object contains new instance variables"""
         self.assertIn("my_number", self.mod1_json.keys())
         self.assertIn("my_name", self.mod1_json.keys())
