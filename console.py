7e23edcc1e95ab9157f6b499457d5fab8e7b687d
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-14 19:15:39 +0000

    base console file

--- console.py
+++ console.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
+"""This is the console file"""
+import cmd
 
+
+class HBNBCommand(cmd.Cmd):
+    """HBNB class inheriting the cmd.Cmd class"""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Handles EOF and terminates the program"""
+        return True
+
+    def emptyline(self):
+        pass
+
+    def postloop(self):
+        print
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
