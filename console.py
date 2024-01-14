0e50d75e2659fb6ebd96d6a009f23176e91d1754
Akachi Anabanti <akachi.anabanti@gmail.com>
2024-01-14 22:24:20 +0000

    updated console file

--- console.py
+++ console.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This is the console file"""
 import cmd
+from models.base_model import BaseModel
+import json
+
+file_path = "file.json"
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,14 +14,16 @@ class HBNBCommand(cmd.Cmd):
         prompt (str): The command prompt.
     """
 
+    __class_names = {"BaseModel": BaseModel}
+
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program.
         """
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """Handles EOF and terminates the program"""
         print("")
         return True
@@ -27,6 +33,135 @@ class HBNBCommand(cmd.Cmd):
         does nothing
         """
         pass
+
+    def do_create(self, line):
+        """
+        Creates a new instance of BaseModel, saves it
+        (to JSON file) and prints the is
+
+        Usage:
+            $ create BaseModel
+        """
+
+        if not line:
+            print("** class name missing **")
+            return False 
+        if len(line.split(" ")) > 1 or line not in self.__class_names.keys():
+            print("** class doesn't exist **")
+            return False
+        my_model_inst = self.__class_names[line]()
+        my_model_inst.save()
+        print(my_model_inst.id)
+
+    def do_show(self, line):
+        """
+        Prints the string representation of 
+        an instance based on the class name and id
+
+        Usage:
+            $ show BaseModel 9uhsu8-8fjhnis89-0vnslwu
+        """
+
+        if not line:
+            print("** class name missing **")
+            return False
+        cmd_args = line.split(" ")
+        class_name = cmd_args[0]
+
+        if class_name not in self.__class_names.keys():
+            print("** class doesn't exist **")
+            return False
+
+        if len(cmd_args) != 2:
+            print("** instance id missing **")
+            return False
+
+        class_instance_id = cmd_args[1]
+
+        instance_key = ".".join([class_name, class_instance_id])
+
+        with open(file_path, "r") as f:
+            objects = json.load(f)
+            if instance_key not in objects.keys():
+                print("** no instance found **")
+                return False
+        obj_dict = objects[instance_key]
+        my_obj_instance = self.__class_names[class_name](**obj_dict)
+        print(my_obj_instance)
+
+    def do_destroy(self, line):
+        """
+        Deletes an instance base on the class name and id (
+        save changes in the JSON file)
+
+        Usage:
+            $ destroy BaseModel 1234-1234-1234
+        """
+
+        if not line:
+            print("** class name missing **")
+            return False
+
+        cmd_args = line.split(" ")
+        class_name = cmd_args[0]
+
+        if class_name not in self.__class_names.keys():
+
+            print("** class doesn't exist **")
+            return False
+
+        if len(cmd_args) != 2:
+
+            print("** instance id missing **")
+            return False
+
+        class_instance_id = cmd_args[1]
+
+        instance_key = ".".join([class_name, class_instance_id])
+
+        with open(file_path, "r") as f:
+            objects = json.load(f)
+            if instance_key not in objects.keys():
+                print("** no instance found **")
+                return False
+
+        del objects[instance_key]
+
+        with open(file_path, "w") as f:
+            json.dump(objects, f)
+
+        self.__class_names[class_name].all().pop(instance_key)
+
+    def do_all(self, line):
+        """
+        Prints all string representation of all instances based
+        or not on the class name
+
+        Usage:
+            $ all BaseModel
+            $ all
+        """
+        obj_list = []
+        with open(file_path, "r") as f:
+            objects = json.load(f)
+        if not line:
+            for obj_key, obj_dict in objects.items():
+                class_name = obj_key.split(".")[0]
+                my_obj_instance = self.__class_names[class_name](**obj_dict)
+                obj_list.append(str(my_obj_instance))
+            print(obj_list)
+
+        elif len(line.split(" ")) > 1 or line not in self.__class_names.keys():
+            print("** class doesn't exist **")
+            return False
+
+        else:
+            for obj_key, obj_dict in objects.items():
+                if obj_key.startswith(line):
+                    class_name = obj_key.split(".")[0]
+                    my_obj_instance = self.__class_names[class_name](**obj_dict)
+                    obj_list.append(str(my_obj_instance))
+            print(obj_list)
 
 
 if __name__ == "__main__":
